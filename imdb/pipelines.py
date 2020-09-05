1d247ceceeb1acdad08d73e1f2b4f03171b9a663
Mostafa Nizar <40121514+Mostafa-Nizar@users.noreply.github.com>
2020-09-05 16:07:32 +0200

    Add files via upload

---
+++ imdb/pipelines.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
+import pymongo
+
+class MongodbPipeline(object):
+    collection_name = "best_movies"
+
+    
+    def open_spider(self,spider):
+        self.client = pymongo.MongoClient("mongodb+srv://mostafa:testtest@cluster0.pmrij.mongodb.net/<dbname>?retryWrites=true&w=majority")
+        self.db = self.client["IMDB"]
+
+    def close_spider(self,spider):    
+        self.client.close()
+    
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
+
+        return item
